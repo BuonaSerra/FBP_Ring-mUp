f198eb08e63d012b0942d1bbd3e7eaa1ee9671db
WesWeCan <79587028+WesWeCan@users.noreply.github.com>
2024-11-25 10:04:37 +0100

    Bugfixes img gen

--- src/front-end/features/LLM_interaction/llm_test.js
+++ src/front-end/features/LLM_interaction/llm_test.js
@@ -1,108 +1,108 @@
-import {prompt_llm, changeInput} from "./generation_prompts.js"; //prompt_llm, reflectionInput, 
-import * as imageGen from '../../features/Image_generation/image_generation';;
+import { prompt_llm, changeInput } from "./generation_prompts.js"; //prompt_llm, reflectionInput, 
+import { postData } from '../../features/Image_generation/image_generation';;
 //import {conversationTranscript} from '../../features/Speech_transcription/transcribings.js'
 
 
 document.getElementById('llm_test').addEventListener('click', llmTest); //start functie llmTest, zodra op de knop is geklikt
 
 async function llmTest() {
-    const url = 'http://127.0.0.1:1234/v1/chat/completions'; //api url + endpoint
+  const url = 'http://127.0.0.1:1234/v1/chat/completions'; //api url + endpoint
 
-    const AgentChoice = document.getElementById("prompt_input").value
-    const inputReflection = document.getElementById("input_reflection").value
-    
-    changeInput(inputReflection);
+  const AgentChoice = document.getElementById("prompt_input").value
+  const inputReflection = document.getElementById("input_reflection").value
 
-    var data = {
-        "model": "gemma-2-27b-it",
-        "messages": [
-            prompt_llm[`${AgentChoice}`],
-        ],
-        "stream": false,
-    };
+  changeInput(inputReflection);
 
-    try {
-        const request2 = new Request(url, {
-    method: "POST",
-    headers: {
+  var data = {
+    "model": "gemma-2-27b-it",
+    "messages": [
+      prompt_llm[`${AgentChoice}`],
+    ],
+    "stream": false,
+  };
+
+  try {
+    const request2 = new Request(url, {
+      method: "POST",
+      headers: {
         "Content-Type": "application/json", //From debugging with GPT-4o;
       },
-    body: JSON.stringify(data),
-  });
-  
-  console.log("LLM test aangeroepen");
+      body: JSON.stringify(data),
+    });
 
-  const response = await fetch(request2);
-  if (!response.ok) {
-    throw new Error(`Response status: ${response.status}`);
+    console.log("LLM test aangeroepen");
+
+    const response = await fetch(request2);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const agent_answer = await response.json();
+    console.log(agent_answer);
+    console.log(agent_answer["choices"]["0"]["message"]["content"]); //locatie van de response
+
+    const agentAnswer = agent_answer["choices"]["0"]["message"]["content"];
+    llmPrompt(agentAnswer);
+    return agentAnswer;
+
+
+
+  } catch (error) {
+    console.error(error.message);
   }
 
-  const agent_answer = await response.json();
-  console.log(agent_answer);
-  console.log(agent_answer["choices"]["0"]["message"]["content"]); //locatie van de response
 
-  const agentAnswer = agent_answer["choices"]["0"]["message"]["content"];
-  llmPrompt(agentAnswer);
-  return agentAnswer;
-  
-  
-  
-} catch (error) {
-  console.error(error.message);
+
 }
 
-    
-     
-  }
 
+async function llmPrompt(agentAnswer) {
+  const url = 'http://127.0.0.1:1234/v1/chat/completions'; //api url + endpoint
 
-  async function llmPrompt(agentAnswer) {
-    const url = 'http://127.0.0.1:1234/v1/chat/completions'; //api url + endpoint
+  var data = {
+    "model": "gemma-2-27b-it",
+    "messages": [
+      { "role": "user", "content": `Answer only in English. Write a prompt to be used for stable diffusion 3, based on the following input: ${agentAnswer} Answer only with the prompt. Answer ONLY with the prompt.` },
+    ],
+    "stream": false,
+  };
 
-    var data = {
-        "model": "gemma-2-27b-it",
-        "messages": [
-            { "role": "user", "content": `Answer only in English. Write a prompt to be used for stable diffusion 3, based on the following input: ${agentAnswer} Answer only with the prompt. Answer ONLY with the prompt.` },
-        ],
-        "stream": false,
-    };
-
-    try {
-        const request3 = new Request(url, {
-    method: "POST",
-    headers: {
+  try {
+    const request3 = new Request(url, {
+      method: "POST",
+      headers: {
         "Content-Type": "application/json", //From debugging with GPT-4o;
       },
-    body: JSON.stringify(data),
-    
-  });
-  
-  console.log("LLM prompt aangeroepen");
+      body: JSON.stringify(data),
 
-  const response = await fetch(request3);
-  if (!response.ok) {
-    throw new Error(`Response status: ${response.status}`);
+    });
+
+    console.log("LLM prompt aangeroepen");
+
+    const response = await fetch(request3);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+    console.log(json["choices"]["0"]["message"]["content"]); //locatie van de response
+
+    const agentPrompt = json["choices"]["0"]["message"]["content"];
+    postData(agentPrompt);
+
+
+
+    return agentPrompt;
+
+
+  } catch (error) {
+    console.error(error.message);
   }
 
-  const json = await response.json();
-  console.log(json);
-  console.log(json["choices"]["0"]["message"]["content"]); //locatie van de response
 
-  const agentPrompt = json["choices"]["0"]["message"]["content"];
-  imageGen.postData(agentPrompt);
 
-   
-  
-  return agentPrompt;
-  
-  
-} catch (error) {
-  console.error(error.message);
 }
-
-    
-     
-  }
 
 
 /*async function llmTest() {
--- src/front-end/features/image_generation/image_generation.js
+++ src/front-end/features/image_generation/image_generation.js
@@ -17,127 +17,143 @@ cfgSlider.addEventListener("input", (event) => {
 
 //open a websocket to be able to get status/execution messages
 export const serverUrl = "127.0.0.1:8188";
-export const clientId = 5;//"agent-client-" + Math.random().toString(36).substring(2, 15);
-export const ws = new WebSocket(`ws://${serverUrl}/ws?clientId=${clientId}`);
-ws.onopen = () => {
-  console.log("WebSocket opened");
+export const clientId = 5; //"agent-client-" + Math.random().toString(36).substring(2, 15);
+let ws;
+
+
+
+function openWebSocket() {
+  ws = new WebSocket(`ws://${serverUrl}/ws?clientId=${clientId}`);
+  ws.onopen = () => {
+    console.log("WebSocket opened");
+  }
+  ws.onclose = () => {
+    console.log("WebSocket closed");
+  };
+  ws.onmessage = (event) => {
+    handleMessage(event.data);
+  };
 }
-ws.onclose = () => {
-  console.log("WebSocket closed");
-};
-ws.onmessage = (event) => {
-  handleMessage(event.data);
-};
+
+
 export async function handleMessage(message) { //receives and sorts any event message; depending on the case as defined below, a certain action is executed.
-      if (typeof message === 'string') {
-          message = JSON.parse(message);
+  if (typeof message === 'string') {
+    message = JSON.parse(message);
+
+    console.log("RAW Message: ", message);
+  }
+  console.log("function aangeroepen " + message.type)
+  switch (message.type) {
+    case 'executing':
+      console.log("Executing...", message.data);
+      if (message.data.node == null) {
+        console.log("Execution done");
       }
-      console.log("function aangeroepen " + message.type)
-      switch (message.type) {
-          case 'executing':
-              console.log("Executing...", message.data);
-              if (message.data.node == null) {
-                  console.log("Execution done");
-                      }
-                  
-                  alert("ComfyUI is done executing");
-              break;
-          case 'execution_start':
-              console.log("Execution started...", message.data);
-              break;
-          case 'excecuted':
-              console.log("Execution of node ", message.data.node, " done");
-              break;
-          case 'status':
-              console.log("Status: ", message.data);
-              console.log("Queue Remaining", message.data.status.exec_info.queue_remaining);
-              if (remainingQueue == 1 && message.data.status.exec_info.queue_remaining == 0) { //AANDACHT: bij gebrek aan message types is dit een oplossing voor later!! Dit is een workaround.
-                console.log('start history');
-                const imagesOutput = (await getImages(responseId));
-                console.log(imagesOutput);
-                convertImage(imagesOutput);
-                //console.log(imageSrc);
-              } else {
-                remainingQueue = message.data.status.exec_info.queue_remaining;
-              }
-              break;
-          case 'execution_cached':
-              console.log("Execution cached...", message.data);
-              break;
-          case 'progress':
-              console.log("Progress: ", `${message.data.value / message.data.max * 100}%`);
-              break;
-          default:
-              console.log("Unknown message type: ", message);
-              break;
-                }
-              }
 
-
-    //asynchrone functie om een post request te sturen naar de api
-  export async function postData(agentPrompt) {
-    /*var promptInput = document.getElementById("prompt_input").value //get the value from the textarea prompt_input
-    var cfgInput = document.getElementById("cfg_input").value //get the value from the number input cfg_input
-    var seedInput = document.getElementById("seed_input") //get the value from the number input seed_input*/
-
-    workflow["6"]["inputs"]["text"] = agentPrompt;
-    //workflow["6"]["inputs"]["text"] = promptInput; //input the prompt value into the right JSON object name
-    //workflow["3"]["inputs"]["cfg"] = cfgInput; //input the cfg value into the right JSON object name
-
-    /*if (seedInput.checked == true){ //checkt of de seedinput checkbox op random staat. Als dat zo is, genereert het een random nummer tussen 1 en 987654321 als seed. Anders is de seed 2.
-      workflow["3"]["inputs"]["seed"] = Math.floor(Math.random() * 9876543210) + 1; 
-    } else {
-      workflow["3"]["inputs"]["seed"] = 2;
-    }*/
-
-    const url = 'http://127.0.0.1:8188/prompt'; //api url + enpoint (/prompt), zie server.py in comfyui om andere endpoints te vinden
-    const clientId = 5//randomUUID() //kan berekend worden, even in duiken
-    try {
-      const p = { prompt: workflow, client_id: clientId }; //combineer workflow met client id
-      const request1 = new Request(url, {
-        method: "POST",
-        body: JSON.stringify(p),
-      });
-      
-      const response = await fetch(request1);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
+      alert("ComfyUI is done executing");
+      break;
+    case 'execution_start':
+      console.log("Execution started...", message.data);
+      break;
+    case 'excecuted':
+      console.log("Execution of node ", message.data.node, " done");
+      break;
+    case 'status':
+      console.log("Status: ", message.data);
+      console.log("Queue Remaining", message.data.status.exec_info.queue_remaining);
+      if (remainingQueue == 1 && message.data.status.exec_info.queue_remaining == 0) { //AANDACHT: bij gebrek aan message types is dit een oplossing voor later!! Dit is een workaround.
+        console.log('start history');
+        const imagesOutput = (await getImages(responseId));
+        console.log(imagesOutput);
+        convertImage(imagesOutput);
+        //console.log(imageSrc);
+      } else {
+        remainingQueue = message.data.status.exec_info.queue_remaining;
       }
-  
-      const json = await response.json();
-      console.log(json);
-      responseId = json['prompt_id']
-      console.log(responseId)
-    } catch (error) {
-      console.error(error.message);
+      break;
+    case 'execution_cached':
+      console.log("Execution cached...", message.data);
+      break;
+    case 'progress':
+      console.log("Progress: ", `${message.data.value / message.data.max * 100}%`);
+      break;
+    default:
+      console.log("Unknown message type: ", message);
+      break;
+  }
+}
+
+
+//asynchrone functie om een post request te sturen naar de api
+export async function postData(agentPrompt) {
+  /*var promptInput = document.getElementById("prompt_input").value //get the value from the textarea prompt_input
+  var cfgInput = document.getElementById("cfg_input").value //get the value from the number input cfg_input
+  var seedInput = document.getElementById("seed_input") //get the value from the number input seed_input*/
+
+  workflow["6"]["inputs"]["text"] = agentPrompt;
+  //workflow["6"]["inputs"]["text"] = promptInput; //input the prompt value into the right JSON object name
+  //workflow["3"]["inputs"]["cfg"] = cfgInput; //input the cfg value into the right JSON object name
+
+  /*if (seedInput.checked == true){ //checkt of de seedinput checkbox op random staat. Als dat zo is, genereert het een random nummer tussen 1 en 987654321 als seed. Anders is de seed 2.
+    workflow["3"]["inputs"]["seed"] = Math.floor(Math.random() * 9876543210) + 1; 
+  } else {
+    workflow["3"]["inputs"]["seed"] = 2;
+  }*/
+
+  const url = 'http://127.0.0.1:8188/prompt'; //api url + enpoint (/prompt), zie server.py in comfyui om andere endpoints te vinden
+  const clientId = 5//randomUUID() //kan berekend worden, even in duiken
+  try {
+    const p = { prompt: workflow, client_id: clientId }; //combineer workflow met client id
+    const request1 = new Request(url, {
+      method: "POST",
+      body: JSON.stringify(p),
+    });
+
+    const response = await fetch(request1);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
     }
 
-    
-     
+    const json = await response.json();
+    console.log(json);
+    responseId = json['prompt_id']
+    console.log(responseId);
+
+    openWebSocket();
+
+
+  } catch (error) {
+    console.error(error.message);
   }
+
+
+
+}
 
 //get history endpoint, which is filled once prompt with promptid is done executing; returns JSON with image location
- export async function getHistory(promptId) {
-    const histurl = 'http://127.0.0.1:8188/history/'+promptId;
-    try {
-            const request2 = new Request(histurl, {
-        method: "GET",
-        body: JSON.stringify(),
-      });
-      
-      const response = await fetch(request2);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-  
-      const json = await response.json();
-      console.log(json);
-      return json;
-      
-    } catch (error) {
-      console.error(error.message);
+export async function getHistory(promptId) {
+
+
+  const histurl = 'http://127.0.0.1:8188/history/' + promptId;
+  try {
+    const request2 = new Request(histurl, {
+      method: "GET",
+      body: JSON.stringify(),
+    });
+
+    const response = await fetch(request2);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
     }
+
+    const json = await response.json();
+    console.log(json);
+    return json;
+
+  } catch (error) {
+    console.error(error.message);
   }
+}
 
 //extract image location from history response, get the image with the getImage function and return arraybuffer from specific image
 export async function getImages(promptId) {
@@ -147,15 +163,15 @@ export async function getImages(promptId) {
   //const nodeOutput = history[promptId];
   //console.log(nodeOutput);
   for (const nodeId in history['outputs']) {
-      const nodeOutput = history['outputs'][nodeId];
-      if ('images' in nodeOutput) {
-          const imagesOutput = [];
-          for (const image of nodeOutput['images']) {
-              const imageData = await getImage(image['filename'], image['subfolder'], image['type']);
-               imagesOutput.push(imageData);
-          }
-          outputImages[nodeId] = imagesOutput;
+    const nodeOutput = history['outputs'][nodeId];
+    if ('images' in nodeOutput) {
+      const imagesOutput = [];
+      for (const image of nodeOutput['images']) {
+        const imageData = await getImage(image['filename'], image['subfolder'], image['type']);
+        imagesOutput.push(imageData);
       }
+      outputImages[nodeId] = imagesOutput;
+    }
   }
   console.log(outputImages);
   return outputImages;
@@ -163,37 +179,37 @@ export async function getImages(promptId) {
 
 //return arraybuffer of specific image based on location
 export async function getImage(filename, subfolder, folderType) {
-      const data = { filename, subfolder, type: folderType };
-      const url = `http://${serverUrl}/view?${new URLSearchParams(data).toString()}`;
-      const response = await fetch(url);
-      return await response.arrayBuffer();
+  const data = { filename, subfolder, type: folderType };
+  const url = `http://${serverUrl}/view?${new URLSearchParams(data).toString()}`;
+  const response = await fetch(url);
+  return await response.arrayBuffer();
 }
 
 //convert arraybuffer to base64 and insert as source into html image display
-export async function convertImage(outputImages){
+export async function convertImage(outputImages) {
   //const imageStuff = "";
   console.log(outputImages);
   for (const nodeId in outputImages) {
     const imageData = outputImages[nodeId];
     for (const image of imageData) {
-        console.log("Image data: ", image); // arrayBuffer
-        // Create a binary string from the ArrayBuffer
-        const binaryString = new Uint8Array(image).reduce((data, byte) => data + String.fromCharCode(byte), '');
-        // Encode the binary string to base64
-        const base64String = btoa(binaryString);
-        // Create the data URL
-        const imageUrl = `data:image/jpeg;base64,${base64String}`;
-        //console.log(imageUrl);
-        //this.callback(imageUrl);
-        //imageStuff = base64String;
-        document.getElementById("generated_image").src = imageUrl;
-        // const imageElement = document.createElement("img");
-        // imageElement.src = imageUrl;
-        // imageElement.style.width = "100%";
-        // imageElement.style.height = "auto";
-        // document.body.appendChild(imageElement);
+      console.log("Image data: ", image); // arrayBuffer
+      // Create a binary string from the ArrayBuffer
+      const binaryString = new Uint8Array(image).reduce((data, byte) => data + String.fromCharCode(byte), '');
+      // Encode the binary string to base64
+      const base64String = btoa(binaryString);
+      // Create the data URL
+      const imageUrl = `data:image/jpeg;base64,${base64String}`;
+      //console.log(imageUrl);
+      //this.callback(imageUrl);
+      //imageStuff = base64String;
+      document.getElementById("generated_image").src = imageUrl;
+      // const imageElement = document.createElement("img");
+      // imageElement.src = imageUrl;
+      // imageElement.style.width = "100%";
+      // imageElement.style.height = "auto";
+      // document.body.appendChild(imageElement);
     }
-    
+
   }
   //return imageStuff;
 }
