4caf590e18304007d7080e04ae85960941ff91ea
BuonaSerra <101342229+BuonaSerra@users.noreply.github.com>
2024-11-27 12:01:17 +0100

    Entire string can be read through serial untill linebreak

--- src/front-end/features/Serial_test/serial_test.js
+++ src/front-end/features/Serial_test/serial_test.js
@@ -1,6 +1,27 @@
 
 
 //code from Google dev example https://developer.chrome.com/docs/capabilities/serial#read-port and electron example https://www.electronjs.org/docs/latest/tutorial/devices#web-serial-api
+//testIt();
+class LineBreakTransformer {
+  constructor() {
+    // A container for holding stream data until a new line.
+    this.chunks = "";
+  }
+
+  transform(chunk, controller) {
+    // Append new chunks to existing chunks.
+    this.chunks += chunk;
+    // For each line breaks in chunks, send the parsed lines out.
+    const lines = this.chunks.split("\r\n");
+    this.chunks = lines.pop();
+    lines.forEach((line) => controller.enqueue(line));
+  }
+
+  flush(controller) {
+    // When the stream is closed, flush any remaining chunks out.
+    controller.enqueue(this.chunks);
+  }
+}
 async function testIt () {
     const filters = [
         { usbVendorId: 0x2341, usbProductId: 0x0042 }
@@ -8,48 +29,55 @@ async function testIt () {
 
     try {
         const port = await navigator.serial.requestPort({ filters })
-        await port.open({ baudRate: 9600 });
+        await port.open({ baudRate: 9600, bufferSize: 1024 });
 
+        //const reader = port.readable.getReader();
         const textDecoder = new TextDecoderStream();
         const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
-        const reader = textDecoder.readable.getReader();
+        const reader = textDecoder.readable
+          .pipeThrough(new TransformStream(new LineBreakTransformer()))
+          .getReader();
+        // Listen to data coming from the serial device.
         while (true) {
-            const { value, done } = await reader.read();
-            if (done) {
-              // Allow the serial port to be closed later.
-              reader.releaseLock();
-              break;
-            }
-            // value is a Uint8Array.
-            console.log(value);
+          const { value, done } = await reader.read();
+          if (done) {
+            // Allow the serial port to be closed later.
+            reader.releaseLock();
+            break;
           }
+          
+          // value is a Uint8Array.
+          console.log(value);
+        }
+        
+        // const textDecoder = new TextDecoderStream();
+        // const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+        // const reader = textDecoder.readable.getReader();
+        // while (true) {
+        //     const { value, done } = await reader.read();
+        //     if (done) {
+        //       // Allow the serial port to be closed later.
+        //       reader.releaseLock();
+        //       break;
+        //     }
+        //     // value is a Uint8Array.
+        //     console.log(value);
+        //   }
         const portInfo = port.getInfo()
-        document.getElementById('device-name').innerHTML = `vendorId: ${portInfo.usbVendorId} | productId: ${portInfo.usbProductId} `
+        //document.getElementById('device-name').innerHTML = `vendorId: ${portInfo.usbVendorId} | productId: ${portInfo.usbProductId} `
       } catch (ex) {
         if (ex.name === 'NotFoundError') {
             console.error('No device found');
-          document.getElementById('device-name').innerHTML = 'Device NOT found'
-        } else {
-          document.getElementById('device-name').innerHTML = ex
-        }
+          //document.getElementById('device-name').innerHTML = 'Device NOT found'
+         } //else {
+        //   document.getElementById('device-name').innerHTML = ex
+        // }
       }
     }
 
     
 
-//     const reader = port.readable.getReader();
-
-// // Listen to data coming from the serial device.
-// while (true) {
-//   const { value, done } = await reader.read();
-//   if (done) {
-//     // Allow the serial port to be closed later.
-//     reader.releaseLock();
-//     break;
-//   }
-//   // value is a Uint8Array.
-//   console.log(value);
-// }
+    
 
 
 document.getElementById('serial_test').addEventListener('click', testIt)
